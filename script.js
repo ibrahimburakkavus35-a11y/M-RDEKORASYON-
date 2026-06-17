4cde1e90d20064b0df4e8a3efe406a8eb45e0d15
ibrahimburakkavus35-a11y <ibrahimburakkavus35@gmail.com>
2026-06-17 15:52:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+console.log("Mir Dekorasyon sitesi aktif.");
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+button.addEventListener("mouseenter", () => {
+button.style.transform = "scale(1.05)";
+button.style.transition = "0.3s";
+});
+
+button.addEventListener("mouseleave", () => {
+button.style.transform = "scale(1)";
+});
+});
+
+});
